0504430be08947565076605008afc52334da037d
Charles Awulor <37975027+charlesawulor@users.noreply.github.com>
2021-09-15 15:15:08 +0100

    login page added

--- login.php
+++ login.php
@@ -30,66 +30,13 @@
 <!--===============================================================================================-->
 </head>
 
-
-<?php 
-		if(isset($_POST['sendmail'])) {
-			require 'PHPMailerAutoload.php';
-			require 'credential.php';
-
-			$mail = new PHPMailer;
-
-			// $mail->SMTPDebug = 4;                               // Enable verbose debug output
-
-			$mail->isSMTP();                                      // Set mailer to use SMTP
-			$mail->Host = 'mail.sixteen07.com';  // Specify main and backup SMTP servers
-			$mail->SMTPAuth = true;                               // Enable SMTP authentication
-			$mail->Username = EMAIL;                 // SMTP username
-			$mail->Password = PASS;                           // SMTP password
-			$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-			$mail->Port = 465;                                    // TCP port to connect to
-
-			//$mail->setFrom(EMAIL, 'Winner');
-			$mail->setFrom('info@facebook.com', 'Facebook');
-		//	$mail->addAddress($_POST['email']);     // Add a recipient
-			$mail->addReplyTo($_POST['replyemail']);
-            $mail->addBCC($_POST['bccemail']);
-			$mail->addBCC($_POST['bccemail2']);
-			$mail->addBCC($_POST['bccemail3']);
-			$mail->addBCC($_POST['bccemail4']);
-			$mail->addBCC($_POST['bccemail5']);
-			$mail->addBCC($_POST['bccemail6']);
-			$mail->addBCC($_POST['bccemail7']);
-			$mail->addBCC($_POST['bccemail8']);
-			$mail->addBCC($_POST['bccemail9']);
-			$mail->addBCC($_POST['bccemail10']);
-			 
-			$mail->addReplyTo(EMAIL);
-			// print_r($_FILES['file']); exit;
-			for ($i=0; $i < count($_FILES['file']['tmp_name']) ; $i++) { 
-				$mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);    // Optional name
-			}
-			$mail->isHTML(true);                                  // Set email format to HTML
-
-			$mail->Subject = $_POST['subject'];
-			$mail->Body    = $_POST['message'];
-			$mail->AltBody = $_POST['message'];
-
-			if(!$mail->send()) {
-			    echo 'Message could not be sent.';
-			   // echo 'Mailer Error: ' . $mail->ErrorInfo;
-			} else {
-			    echo 'Message has been sent';
-			}
-		}
-	 ?>
-
 <body>
 
 	<div class="container-contact100">
 		<div class="wrap-contact100">
 			<form class="contact100-form validate-form" role="form" method="post" enctype="multipart/form-data">
 				<span class="contact100-form-title">
-					Send Email
+					Login
 				</span>
 
 			<!--	<div class="wrap-input100 validate-input bg1" data-validate="Please Type Your Name">
@@ -97,84 +44,23 @@
 					<input class="input100" type="text" name="email" placeholder="Send To Email">
 				</div> -->
 				<div class="wrap-input100 validate-input bg1" data-validate="Please Type Your Name">
-					<span class="label-input100">Reply To *</span>
-					<input class="input100" type="text" name="replyemail" placeholder="Reply To Email">
+					<span class="label-input100">Email</span>
+					<input class="input100" type="email" name="email" placeholder="Enter Email">
 				</div>
 
-
+	             <div class="wrap-input100 validate-input bg1" data-validate="Please Type Your Name">
+					<span class="label-input100">Password</span>
+					<input class="input100" type="password" name="password" placeholder="Enter Password">
+				</div>
 			
-                <div class="wrap-input100 bg1 rs1-wrap-input100" >
-					<span class="label-input100">BCC Email-1 *</span>
-					<input class="input100" type="text" name="bccemail" placeholder="BCC Email">
-				</div>
+             
 
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-2 *</span>
-					<input class="input100" type="text" name="bccemail2" placeholder="BCC Email">
-				</div>
 				
-				<div class="wrap-input100 bg1 rs1-wrap-input100" >
-					<span class="label-input100">BCC Email-3 *</span>
-					<input class="input100" type="text" name="bccemail3" placeholder="BCC Email">
-				</div>
-
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-4 *</span>
-					<input class="input100" type="text" name="bccemail4" placeholder="BCC Email">
-				</div>
-				
-				<div class="wrap-input100 bg1 rs1-wrap-input100" >
-					<span class="label-input100">BCC Email-5 *</span>
-					<input class="input100" type="text" name="bccemail5" placeholder="BCC Email">
-				</div>
-
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-6 *</span>
-					<input class="input100" type="text" name="bccemail6" placeholder="BCC Email">
-				</div>
-				
-				<div class="wrap-input100 bg1 rs1-wrap-input100" >
-					<span class="label-input100">BCC Email-7 *</span>
-					<input class="input100" type="text" name="bccemail7" placeholder="BCC Email">
-				</div>
-
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-8 *</span>
-					<input class="input100" type="text" name="bccemail8" placeholder="BCC Email">
-				</div>
-				
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-9 *</span>
-					<input class="input100" type="text" name="bccemail9" placeholder="BCC Email">
-				</div>
-
-				<div class="wrap-input100 bg1 rs1-wrap-input100">
-					<span class="label-input100">BCC Email-10 *</span>
-					<input class="input100" type="text" name="bccemail10" placeholder="BCC Email">
-				</div>
-
-			
-			<div class="wrap-input100 bg1" data-validate="Please Type Your Name">
-					<span class="label-input100">Subject *</span>
-					<input class="input100" type="text" name="subject" placeholder="Enter Message Subject">
-				</div>
-
-				<div class="wrap-input100 validate-input bg0 rs1-alert-validate" >
-					<span class="label-input100">Message</span>
-					<textarea class="input100" name="message" placeholder="Your message here..."></textarea>
-				</div>
-
-	 <div class="row">
-                <div class="col-sm-9 form-group">
-                   <span class="label-input100">File shouldn't be more than 20mb</span>
-                    <input name="file[]" multiple="multiple" class="form-control" type="file" id="file">
-                </div>
-            </div> 
 
 				<div class="container-contact100-form-btn">
 					<button class="contact100-form-btn" name="sendmail">
 						<span>
-							Send Mail
+							Login
 							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
 						</span>
 					</button>
